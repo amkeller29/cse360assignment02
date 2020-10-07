006c88e0973f0dad136045695f2495af5c718d17
amkeller29 <72528882+amkeller29@users.noreply.github.com>
2020-10-07 15:57:23 -0700

    Create AddingMachine.java

---
+++ AddingMachine.java
@@ -0,0 +1,38 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  private String string = "";
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () 
+  {
+    return total;
+  }
+  
+  public void add (int value) 
+  {
+    total = total + value;
+    string = string + "+" + value;
+  }
+
+  public void subtract (int value) 
+  {
+    total = total - value;
+    string = string + "-" + value;
+  }
+
+  public String toString () 
+  {
+    return string;
+  }
+
+  public void clear() 
+  {
+    total = 0;
+    string = "";
+  }
+}
